33631f6d4100df8b4feec9194f7374ed22797b4c
kingwangz <540886982@qq.com>
2017-08-02 11:24:55 +0800

    king1

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -3,8 +3,12 @@ package king1;
 public class koing1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("king wang ");
+		byte mybyte=20;
+		short myshort=2000;
+		int myint=20000;
+		long mylong=2000000;
+		long my=mybyte+myshort+myint+mylong;
+		System.out.println("king wang "+my);
 
 	}
 
