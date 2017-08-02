ebee596bb075155ed17cccd8e375ab72a295ce7e
kingwangz <540886982@qq.com>
2017-08-02 14:21:48 +0800

    king1

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -5,11 +5,14 @@ public class koing1 {
 	public static void main(String[] args) {
 		char my='k';char myf='*';
 		int p=9303838;int k=342934;
+		char c='\\';char v='\u2605';
 		System.out.println("king wang "+(int)my);
 		System.out.println("king wang "+(int)myf);
 		System.out.println("king wang "+(char)p);
 		System.out.println("king wang "+(char)k);
 		System.out.println("king wang "+(int)k);
+		System.out.println("king wang "+c);
+		System.out.println("king wang "+v);
 	}
 
 }
