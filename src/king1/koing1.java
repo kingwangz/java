b15ebd9a0030a1eb6ce40cd042ff1f937f709825
kingwangz <540886982@qq.com>
2017-08-02 16:58:59 +0800

    king1

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,13 +2,14 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		int a,b;
-		a=4;
+		int b;
+		int a=(int)4.6;
 		b=5;
 		boolean r=((a>b))&&((a!=b));
 		int c=a<b?8:9;
 		System.out.println("king wang "+r);
 		System.out.println("king wang "+c);
+		System.out.println("king wang "+a);
 	}
 
 }
