299579341dff873db7854c4eacaa3a1ef2d70a02
kingwangz <540886982@qq.com>
2017-08-02 18:44:57 +0800

    king1

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,15 +2,16 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		int math=95;
-		int ent=56;
-		if(math>60) {
+		int math = 95;
+		int ent = 56;
+		if (math > 60) {
 			System.out.println("pass");
-		}else {System.out.println("not");
-	}
-		if(ent>60) {
+		} else {
+			System.out.println("not");
+		}
+		if (ent > 60) {
 			System.out.println("pass");
-		}else {
+		} else {
 			System.out.println("not");
 		}
 	}
