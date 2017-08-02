52f772ffb72937885ed93b32ff6d1e92cacba1db
kingwangz <540886982@qq.com>
2017-08-02 16:16:21 +0800

    king1

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,17 +1,13 @@
 package king1;
 
 public class koing1 {
-	static final double PI=3.14;
-	static int age=22;
 	public static void main(String[] args) {
-		final int number;int a, b;
-		a=77;
-		number=123;
-		age=88;
-		b=a+age;
-		System.out.println("king wang "+age);
-		System.out.println("king wang "+number);
-		System.out.println("king wang "+b*number);
+		int a,b,c;
+		a=4;
+		b=a++;
+		c=++a;
+		System.out.println("king wang "+b);
+		System.out.println("king wang "+c);
 	}
 
 }
