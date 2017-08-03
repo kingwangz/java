818de19f2a35a5b58c1ff0d065fb85c9b8039075
kingwangz <540886982@qq.com>
2017-08-03 19:03:53 +0800

    instem delete

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,23 +2,10 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		String d = "";
-		long stime = System.currentTimeMillis();
-		for (int i = 0; i < 100000; i++) {
-			d = d + i;
-			System.out.println(d);
-		}
-		long etime = System.currentTimeMillis();
-		long time = etime - stime;
-		System.out.println(time);
-		StringBuilder builder = new StringBuilder("");
-		stime = System.currentTimeMillis();
-		for (int i = 0; i < 100000; i++) {
-			builder.append(i);
-			System.out.println(builder);
-		}
-		etime = System.currentTimeMillis();
-		time = etime - stime;
-		System.out.println(time);
+		StringBuilder b = new StringBuilder("holle");
+		b.insert(5, " word");
+		System.out.println(b);
+		b.delete(5, 10);
+		System.out.println(b);
 	}
 }
