c0f55af4cf4bcf0aa0fe5806d6136f8186ce5426
kingwangz <540886982@qq.com>
2017-08-03 11:10:29 +0800

    continue

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,13 +2,13 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		look: for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 6; j++) {
-				if (j == 4) {
-					break look;
-				}
-				System.out.println("i=" + i + "j=" + j);
+		int i = 1;
+		while (i < 6) {
+			if (i == 3) {
+				continue;
 			}
+			i++;
+			System.out.println(i);
 		}
 	}
 }
