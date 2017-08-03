8fa76b675a3e63a3073cb900453f46f876f5c9e7
kingwangz <540886982@qq.com>
2017-08-03 15:53:26 +0800

    trim

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,14 +2,18 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		String s = "we are students";
+		String s = " we are students";
 		int size1 = s.lastIndexOf(" ");
 		int size2 = s.indexOf("a");
 		char k = s.charAt(5);
 		String l = s.substring(0, 5);
+		String m = s.trim();
 		System.out.println(size1);
 		System.out.println(size2);
 		System.out.println(k);
 		System.out.println(l);
+		System.out.println(s.length());
+		System.out.println(m.length());
+		System.out.println(m);
 	}
 }
