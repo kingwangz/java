8ff04759000e45700d391356f82dfb4fcb334201
kingwangz <540886982@qq.com>
2017-08-03 16:52:54 +0800

    equals equalsIgnorecase

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,24 +2,14 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		String s = " we are students";
-		int size1 = s.lastIndexOf(" ");
-		int size2 = s.indexOf("a");
-		char k = s.charAt(5);
-		String l = s.substring(0, 5);
-		String m = s.trim();
-		System.out.println(size1);
-		System.out.println(size2);
-		System.out.println(k);
+		String tom = "holle";
+		String kl = "holle";
+		String p = "Holle";
+		boolean l = (tom == kl);
 		System.out.println(l);
-		System.out.println(s.length());
-		System.out.println(m.length());
-		System.out.println(m);
-		String n = s.replace("w", "W");
-		System.out.println(n);
-		boolean b = s.startsWith(" e");
-		boolean b1 = s.endsWith("ts");
-		System.out.println(b);
-		System.out.println(b1);
+		boolean o = tom.equals(kl);
+		System.out.println(o);
+		boolean u = tom.equalsIgnoreCase(p);
+		System.out.println(u);
 	}
 }
