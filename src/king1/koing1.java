f02c224abac1b3923be2b45d4475e73ddc64d29a
kingwangz <540886982@qq.com>
2017-08-03 17:06:25 +0800

    compareTo

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,14 +2,10 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		String tom = "holle";
-		String kl = "holle";
-		String p = "Holle";
-		boolean l = (tom == kl);
-		System.out.println(l);
-		boolean o = tom.equals(kl);
-		System.out.println(o);
-		boolean u = tom.equalsIgnoreCase(p);
-		System.out.println(u);
+		String n = "a";
+		String k = "c";
+		System.out.println(n.compareTo(k));
+		n = "f";
+		System.out.println(n.compareTo(k));
 	}
 }
