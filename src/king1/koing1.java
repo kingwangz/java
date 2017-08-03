c71147b5f261f59f49ec10ebaaafccd8e43c376f
kingwangz <540886982@qq.com>
2017-08-03 11:02:19 +0800

    break

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,9 +2,13 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		int s[] = { 1, 2, 3 };
-		for (int x : s) {
-			System.out.println(x);
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 6; j++) {
+				if (j == 4) {
+					break;
+				}
+				System.out.println("i=" + i + "j=" + j);
+			}
 		}
 	}
 }
