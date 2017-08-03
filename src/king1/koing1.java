3c93ca5754e7aaa4b0cdacc1d70b08434106cdb3
kingwangz <540886982@qq.com>
2017-08-03 15:44:26 +0800

    substring

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -6,8 +6,10 @@ public class koing1 {
 		int size1 = s.lastIndexOf(" ");
 		int size2 = s.indexOf("a");
 		char k = s.charAt(5);
+		String l = s.substring(0, 5);
 		System.out.println(size1);
 		System.out.println(size2);
 		System.out.println(k);
+		System.out.println(l);
 	}
 }
