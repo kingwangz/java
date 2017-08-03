56de26e5a5daa083f2ec1fbdebf0bf999349de5f
kingwangz <540886982@qq.com>
2017-08-03 18:16:50 +0800

    format time

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,15 +1,25 @@
 package king1;
 
+import java.util.Date;
+
 public class koing1 {
 	public static void main(String[] args) {
-		String s = "klk.klk.dff";
-		String[] f = s.split("\\.");
-		String[] k = s.split("\\.", 2);
-		for (String a : f) {
-			System.out.println(a);
-		}
-		for (String a : k) {
-			System.out.println(a);
-		}
+		Date date = new Date();
+		String year = String.format("%tY", date);
+		String month = String.format("%tB", date);
+		String day = String.format("%td", date);
+		System.out.println(year);
+		System.out.println(month);
+		System.out.println(day);
+		String hour = String.format("%tH", date);
+		String minute = String.format("%tM", date);
+		String second = String.format("%tS", date);
+		System.out.println(hour);
+		System.out.println(minute);
+		System.out.println(second);
+		String time = String.format("%tc", date);
+		String form = String.format("%tF", date);
+		System.out.println(time);
+		System.out.println(form);
 	}
 }
