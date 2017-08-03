e9e33a34163cd285296632d1f30c94e74e9628c1
kingwangz <540886982@qq.com>
2017-08-03 18:54:08 +0800

    生成器stringBuilder

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,18 +2,21 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		String r = "\\w+@\\w+(\\.\\w{2,3})*\\.\\w{2,3}";
-		String a = "aaa@";
-		String a1 = "aaaa";
-		String a2 = "111@qq.com.cn";
-		if (a.matches(r)) {
-			System.out.println("pass1");
+		String d = "";
+		long stime = System.currentTimeMillis();
+		for (int i = 0; i < 100000; i++) {
+			d = d + i;
 		}
-		if (a1.matches(r)) {
-			System.out.println("pass2");
+		long etime = System.currentTimeMillis();
+		long time = etime - stime;
+		System.out.println(time);
+		StringBuilder builder = new StringBuilder("");
+		stime = System.currentTimeMillis();
+		for (int i = 0; i < 100000; i++) {
+			builder.append(i);
 		}
-		if (a2.matches(r)) {
-			System.out.println("pass3");
-		}
+		etime = System.currentTimeMillis();
+		time = etime - stime;
+		System.out.println(time);
 	}
 }
