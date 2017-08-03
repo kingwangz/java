fa437bc6aa8d817cd84c36b8700c30cf00df482e
kingwangz <540886982@qq.com>
2017-08-03 14:37:19 +0800

    string

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,11 +2,11 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		for (int i = 1; i < 20; i++) {
-			if (i % 2 == 0) {
-				continue;
-			}
-			System.out.println(i);
-		}
+		String s1 = new String("hello");
+		String s2 = new String("word");
+		String s = s1 + " " + s2;
+		System.out.println(s);
+		String h = "hello word";
+		System.out.println(h);
 	}
 }
