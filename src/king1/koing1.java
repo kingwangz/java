41ce680d4a76d92e6448545d49f1bfdf778bcd0e
kingwangz <540886982@qq.com>
2017-08-03 10:40:35 +0800

    foreach

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,10 +2,9 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		int s = 0;
-		for (int i = 2; i <= 100; i += 2) {
-			s = s + i;
+		int s[] = { 1, 2, 3 };
+		for (int x : s) {
+			System.out.println(x);
 		}
-		System.out.println("sum=" + s);
 	}
 }
