4d00eaa92106990216600322026f1205c3a51a8f
kingwangz <540886982@qq.com>
2017-08-04 15:20:45 +0800

    print

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,13 +2,17 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		int a[][] = new int[3][4];
-		for (int i = 0; i < a.length; i++) {
-			for (int j = 0; j < a[i].length; j++) {
-				System.out.print(a[i][j]);
+		int b[][] = new int[10000][10000];
+		long stime = System.currentTimeMillis();
+		for (int i = 0; i < b.length; i++) {
+			for (int j = 0; j < b[i].length; j++) {
+				System.out.print(b[i][j]);
 			}
 			System.out.println();
 		}
+		long etime = System.currentTimeMillis();
+		long time = etime - stime;
+		System.out.println(time);
 
 	}
 }
