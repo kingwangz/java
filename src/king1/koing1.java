9eeee00ae5b3187ad485d282260c99241bafc4d5
kingwangz <540886982@qq.com>
2017-08-04 14:26:02 +0800

    二维数组

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,13 +2,13 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		int day[] = new int[] { 1, 2, 3, 4, 5 };
-		for (int i = 0; i < 5; i++) {
-			System.out.println((i + 1) + " " + day[i]);
+		int a[][] = new int[3][4];
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				System.out.print(a[i][j]);
+			}
+			System.out.println();
 		}
-		String str[] = new String[] { "a", "b", "c" };
-		for (int i = 0; i < 3; i++) {
-			System.out.println((i + 1) + " " + str[i]);
-		}
+
 	}
 }
