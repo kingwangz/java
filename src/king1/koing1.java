063d5ef7cff38881966b90924047c959504dc0d1
kingwangz <540886982@qq.com>
2017-08-04 19:16:11 +0800

    类

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,33 +1,21 @@
 package king1;
 
 public class koing1 {
-	public static void main(String[] args) {
-		int array[] = new int[] { 34, 32, 23, 56, 78, 67 };
-		koing1 sorter = new koing1();
-		sorter.sort(array);
+	private String name;
+
+	public String getname() {
+		int id = 0;
+		setname("java");
+		return id + this.name;
 	}
 
-	public void sort(int[] array) {
-		System.out.println("y");
-		showArray(array);
-		int temp;
-		int len = array.length;
-		for (int i = 1; i < len / 2; i++) {
-
-			temp = array[i];
-			array[i] = array[len - 1 - i];
-			array[len - 1 - i] = temp;
-		}
-		System.out.println("x");
-		showArray(array);
+	public void setname(String name) {
+		this.name = name;
 
 	}
 
-	public void showArray(int[] array) {
-		for (int i : array) {
-			System.out.print("\t" + i);
-		}
-		System.out.println();
+	public koing1 nambookname() {
+		return this;
 
 	}
 }
