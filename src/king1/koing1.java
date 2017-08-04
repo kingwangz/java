dbf0ea3c201eba48478b1374d6b9b4b1aa2dc246
kingwangz <540886982@qq.com>
2017-08-04 15:37:07 +0800

    Arrays.sort

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class koing1 {
 	public static void main(String[] args) {
-		int a[] = new int[] { 1, 2, 3, 4, 5 };
-		Arrays.fill(a, 1, 3, 8);
+		int a[] = new int[] { 1, 6, 8, 4, 5 };
+		Arrays.sort(a);
 		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
 		}
