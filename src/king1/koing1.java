d6ad3e1443f9b823a097167961b23dc8bf0c2f18
kingwangz <540886982@qq.com>
2017-08-04 17:01:57 +0800

    冒泡排序

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,15 +1,31 @@
 package king1;
 
-import java.util.Arrays;
-
 public class koing1 {
 	public static void main(String[] args) {
-		int a[] = new int[] { 1, 6, 8, 4, 5 };
-		Arrays.sort(a);
-		for (int i = 0; i < a.length; i++) {
-			System.out.println(a[i]);
+		int array[] = new int[] { 34, 32, 23, 56, 78, 67 };
+		koing1 sorter = new koing1();
+		sorter.sort(array);
+	}
+
+	public void sort(int[] array) {
+		for (int i = 1; i < array.length; i++) {
+			for (int j = 0; j < array.length - i; j++) {
+				if (array[j] > array[j + 1]) {
+					int temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
 		}
-		int b = Arrays.binarySearch(a, 0, 4, 4);
-		System.out.println(b);
+		showArray(array);
+
+	}
+
+	public void showArray(int[] array) {
+		for (int i : array) {
+			System.out.print(">" + i);
+		}
+		System.out.println();
+
 	}
 }
