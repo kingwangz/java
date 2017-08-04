5be46693947424dc27b7611dd26912ba467569b7
kingwangz <540886982@qq.com>
2017-08-04 15:49:37 +0800

    Arrays.copyOfRange

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class koing1 {
 	public static void main(String[] args) {
 		int a[] = new int[] { 1, 6, 8, 4, 5 };
-		int n[] = Arrays.copyOf(a, 8);
+		int n[] = Arrays.copyOfRange(a, 1, 3);
 		for (int i = 0; i < n.length; i++) {
 			System.out.println(n[i]);
 		}
