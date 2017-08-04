8f7415addf267cc5a5ac408cb5b1b2a0903a9399
kingwangz <540886982@qq.com>
2017-08-04 15:35:58 +0800

    Arrays.fill

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,18 +1,13 @@
 package king1;
 
+import java.util.Arrays;
+
 public class koing1 {
 	public static void main(String[] args) {
-		int b[][] = new int[10000][10000];
-		long stime = System.currentTimeMillis();
-		for (int i = 0; i < b.length; i++) {
-			for (int j = 0; j < b[i].length; j++) {
-				System.out.print(b[i][j]);
-			}
-			System.out.println();
+		int a[] = new int[] { 1, 2, 3, 4, 5 };
+		Arrays.fill(a, 1, 3, 8);
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
 		}
-		long etime = System.currentTimeMillis();
-		long time = etime - stime;
-		System.out.println(time);
-
 	}
 }
