37cc6e4903ae07a226b7b6bfa65de673d1d4db36
kingwangz <540886982@qq.com>
2017-08-04 17:55:51 +0800

    反转排序

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -8,22 +8,24 @@ public class koing1 {
 	}
 
 	public void sort(int[] array) {
-		for (int i = 1; i < array.length; i++) {
-			for (int j = 0; j < array.length - i; j++) {
-				if (array[j] > array[j + 1]) {
-					int temp = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = temp;
-				}
-			}
+		System.out.println("y");
+		showArray(array);
+		int temp;
+		int len = array.length;
+		for (int i = 1; i < len / 2; i++) {
+
+			temp = array[i];
+			array[i] = array[len - 1 - i];
+			array[len - 1 - i] = temp;
 		}
+		System.out.println("x");
 		showArray(array);
 
 	}
 
 	public void showArray(int[] array) {
 		for (int i : array) {
-			System.out.print(">" + i);
+			System.out.print("\t" + i);
 		}
 		System.out.println();
 
