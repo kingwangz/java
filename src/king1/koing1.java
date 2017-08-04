670cbf565af96cb4539aa900ee20c02d100db452
kingwangz <540886982@qq.com>
2017-08-04 15:58:38 +0800

    Arrays.binarySearch

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -5,9 +5,8 @@ import java.util.Arrays;
 public class koing1 {
 	public static void main(String[] args) {
 		int a[] = new int[] { 1, 6, 8, 4, 5 };
-		int n[] = Arrays.copyOfRange(a, 1, 3);
-		for (int i = 0; i < n.length; i++) {
-			System.out.println(n[i]);
-		}
+		Arrays.sort(a);
+		int b = Arrays.binarySearch(a, 5);
+		System.out.println(b);
 	}
 }
