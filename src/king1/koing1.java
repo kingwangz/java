84c6bfaa0dea4a4a174c9ac84edd9e650c1ec9ee
kingwangz <540886982@qq.com>
2017-08-04 10:55:22 +0800

    一维数组

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,10 +2,13 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		StringBuilder b = new StringBuilder("holle");
-		b.insert(5, " word");
-		System.out.println(b);
-		b.delete(5, 10);
-		System.out.println(b);
+		int day[] = new int[] { 1, 2, 3, 4, 5 };
+		for (int i = 0; i < 5; i++) {
+			System.out.println((i + 1) + " " + day[i]);
+		}
+		String str[] = new String[] { "a", "b", "c" };
+		for (int i = 0; i < 3; i++) {
+			System.out.println((i + 1) + " " + str[i]);
+		}
 	}
 }
