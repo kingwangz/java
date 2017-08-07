d4a8b8f3e80dfb5c5907198edf8e848fd68548d0
kingwangz <540886982@qq.com>
2017-08-07 19:09:54 +0800

    Byte

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,9 +2,11 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		Boolean b1 = new Boolean(true);
-		Boolean b2 = new Boolean("ok");
-		System.out.println(b1.booleanValue());
-		System.out.println(b2.booleanValue());
+		byte k1 = Byte.MAX_VALUE;
+		byte k2 = Byte.MIN_VALUE;
+		int k3 = Byte.SIZE;
+		System.out.println(k1);
+		System.out.println(k2);
+		System.out.println(k3);
 	}
 }
