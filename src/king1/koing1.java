53ef1141198b1556f5d30babb9e8d8d078898d12
kingwangz <540886982@qq.com>
2017-08-07 19:15:58 +0800

    Double

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,9 +2,9 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		Character m1 = new Character('a');
-		Character m2 = new Character('A');
-		System.out.println(Character.isUpperCase(m1));
-		System.out.println(Character.isLowerCase(m2));
+		double k1 = Double.MAX_VALUE;
+		double k2 = Double.MIN_NORMAL;
+		System.out.println(k1);
+		System.out.println(k2);
 	}
 }
