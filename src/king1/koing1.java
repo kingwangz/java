1bc6b2295eaf6cc3dac46b3d782a22523d3b7a8a
kingwangz <540886982@qq.com>
2017-08-07 19:03:17 +0800

    Integer

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,13 +2,11 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		String s1 = Integer.toString(456);
-		String s2 = Integer.toBinaryString(456);
-		String s3 = Integer.toHexString(456);
-		String s4 = Integer.toOctalString(456);
-		System.out.println("10 " + s1);
-		System.out.println("2 " + s2);
-		System.out.println("16 " + s3);
-		System.out.println("8 " + s4);
+		int max = Integer.MAX_VALUE;
+		int min = Integer.MIN_VALUE;
+		int ints = Integer.SIZE;
+		System.out.println(max);
+		System.out.println(min);
+		System.out.println(ints);
 	}
 }
