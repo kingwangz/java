e50ae69f36f4da84e32e37be8cd9df35ed36be96
kingwangz <540886982@qq.com>
2017-08-07 18:41:35 +0800

    Integer.to

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,12 +2,13 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		String k[] = { "98", "88", "78", "66" };
-		int sum = 0;
-		for (int i = 0; i < k.length; i++) {
-			int my = Integer.parseInt(k[i]);
-			sum = sum + my;
-		}
-		System.out.println(sum);
+		String s1 = Integer.toString(456);
+		String s2 = Integer.toBinaryString(456);
+		String s3 = Integer.toHexString(456);
+		String s4 = Integer.toOctalString(456);
+		System.out.println("10 " + s1);
+		System.out.println("2 " + s2);
+		System.out.println("16 " + s3);
+		System.out.println("8 " + s4);
 	}
 }
