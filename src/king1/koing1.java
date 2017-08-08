b8e71c200ba7b49c06b9ff8b8953ca5f0178cd76
kingwangz <540886982@qq.com>
2017-08-08 14:48:17 +0800

    Math.random

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,10 +1,19 @@
 package king1;
 
 public class koing1 {
+	public static int k(double num1, double num2) {
+		int s = (int) num1 + (int) (Math.random() * (num2 - num1));
+		return s;
+	}
+
+	public static char n(char n1, char n2) {
+		char n = (char) (n1 + Math.random() * (n2 - n1));
+		return n;
+	}
 
 	public static void main(String[] args) {
-		System.out.println("4,8max " + Math.max(4, 8));
-		System.out.println("4,8min  " + Math.min(4, 8));
-		System.out.println("-7 " + Math.abs(-7));
+		System.out.println(k(2, 10));
+		System.out.println(n('a', 'z'));
+		System.out.println(n('0', '9'));
 	}
 }
