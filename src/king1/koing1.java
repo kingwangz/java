471823420c6627f48edd0659262af93be7939393
kingwangz <540886982@qq.com>
2017-08-08 11:07:02 +0800

    Math

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,16 +1,11 @@
 package king1;
 
-import java.text.DecimalFormat;
-
 public class koing1 {
 
 	public static void main(String[] args) {
-		DecimalFormat kDecimalFormat = new DecimalFormat();
-		kDecimalFormat.setGroupingSize(2);
-		String out = kDecimalFormat.format(1234567.89);
-		System.out.println(out);
-		kDecimalFormat.setGroupingUsed(false);
-		String out2 = kDecimalFormat.format(12345678.78);
-		System.out.println(out2);
+		System.out.println("sin90 " + Math.sin(Math.PI / 2));
+		System.out.println("cos0 " + Math.cos(0));
+		System.out.println("tan90 " + Math.tan(Math.PI / 2));
+		System.out.println("120 " + Math.toRadians(120));
 	}
 }
