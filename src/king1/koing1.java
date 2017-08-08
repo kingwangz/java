9e4ebd093ac787cdf7811c71ba705d4a2301f71f
kingwangz <540886982@qq.com>
2017-08-08 10:44:37 +0800

    setGroupingSize

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -3,24 +3,14 @@ package king1;
 import java.text.DecimalFormat;
 
 public class koing1 {
-	static public void k(String pattern, double value) {
-		DecimalFormat my = new DecimalFormat(pattern);
-		String out = my.format(value);
-		System.out.println(value + " " + pattern + " " + out);
-	}
-
-	static public void n(String pattern, double value) {
-		DecimalFormat my = new DecimalFormat();
-		my.applyPattern(pattern);
-		System.out.println(value + " " + pattern + " " + my.format(value));
-	}
 
 	public static void main(String[] args) {
-		k("###,###.###", 12346.789);
-		k("0000000.######kg", 12345678.98);
-		k("0000.000", 122.34);
-		n("#.##%", 0.789);
-		n("000.###", 123456.78);
-		n("0.00\u2030", 0.789);
+		DecimalFormat kDecimalFormat = new DecimalFormat();
+		kDecimalFormat.setGroupingSize(2);
+		String out = kDecimalFormat.format(1234567.89);
+		System.out.println(out);
+		kDecimalFormat.setGroupingUsed(false);
+		String out2 = kDecimalFormat.format(12345678.78);
+		System.out.println(out2);
 	}
 }
