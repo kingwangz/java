9d67eaaf3452ae199006779ef75334a537890446
kingwangz <540886982@qq.com>
2017-08-08 16:01:46 +0800

    BigDecimal

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,16 +1,48 @@
 package king1;
 
-import java.math.BigInteger;;
+import java.math.BigDecimal;
 
 public class koing1 {
+	static final int lo = 10;
+
+	public BigDecimal add(double v1, double v2) {
+		BigDecimal b1 = new BigDecimal(Double.toString(v1));
+		BigDecimal b2 = new BigDecimal(Double.toString(v2));
+		return b1.add(b2);
+	}
+
+	public BigDecimal sub(double v1, double v2) {
+		BigDecimal b1 = new BigDecimal(Double.toString(v1));
+		BigDecimal b2 = new BigDecimal(Double.toString(v2));
+		return b1.subtract(b2);
+	}
+
+	public BigDecimal mul(double v1, double v2) {
+		BigDecimal b1 = new BigDecimal(Double.toString(v1));
+		BigDecimal b2 = new BigDecimal(Double.toString(v2));
+		return b1.multiply(b2);
+	}
+
+	public BigDecimal div(double v1, double v2) {
+		return div(v1, v2, lo);
+	}
+
+	public BigDecimal div(double v1, double v2, int b) {
+		if (b < 0) {
+			System.out.println("k");
+		}
+		BigDecimal b1 = new BigDecimal(Double.toString(v1));
+		BigDecimal b2 = new BigDecimal(Double.toString(v2));
+		return b1.divide(b2, b, BigDecimal.ROUND_HALF_UP);
+	}
+
 	public static void main(String[] args) {
-		BigInteger n = new BigInteger("4");
-		System.out.println(n.add(new BigInteger("2")));
-		System.out.println(n.subtract(new BigInteger("2")));
-		System.out.println(n.multiply(new BigInteger("2")));
-		System.out.println(n.divide(new BigInteger("2")));
-		System.out.println(n.divideAndRemainder(new BigInteger("3"))[1]);
-		System.out.println(n.pow(2));
-		System.out.println(n.negate());
+		koing1 n = new koing1();
+		System.out.println(n.add(-7.5, 8.9));
+		System.out.println(n.sub(-7.5, 8.9));
+		System.out.println(n.mul(-7.5, 8.9));
+		System.out.println(n.div(10, 2));
+		System.out.println(n.div(-7.5, 8.9, 5));
+
 	}
 }
