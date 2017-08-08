56179df731d2c3c93a20ae6c9f55ce0cd40da9cc
kingwangz <540886982@qq.com>
2017-08-08 14:53:54 +0800

    Random

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,19 +1,14 @@
 package king1;
 
+import java.util.Random;
+
 public class koing1 {
-	public static int k(double num1, double num2) {
-		int s = (int) num1 + (int) (Math.random() * (num2 - num1));
-		return s;
-	}
-
-	public static char n(char n1, char n2) {
-		char n = (char) (n1 + Math.random() * (n2 - n1));
-		return n;
-	}
-
 	public static void main(String[] args) {
-		System.out.println(k(2, 10));
-		System.out.println(n('a', 'z'));
-		System.out.println(n('0', '9'));
+		Random n = new Random();
+		System.out.println(n.nextInt());
+		System.out.println(n.nextInt(100));
+		System.out.println(n.nextFloat());
+		System.out.println(n.nextDouble());
+		System.out.println(n.nextBoolean());
 	}
 }
