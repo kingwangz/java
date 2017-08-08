4b6cffe1678fa165ad0d98e80519f34011843157
kingwangz <540886982@qq.com>
2017-08-08 11:22:17 +0800

    Math

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -3,11 +3,8 @@ package king1;
 public class koing1 {
 
 	public static void main(String[] args) {
-		System.out.println("ceil " + Math.ceil(5.2));
-		System.out.println("floor " + Math.floor(5.5));
-		System.out.println("rint " + Math.rint(2.7));
-		System.out.println("rint " + Math.rint(2.5));
-		System.out.println("round " + Math.round(3.4));
-		System.out.println("round " + Math.round(2.5f));
+		System.out.println("4,8max " + Math.max(4, 8));
+		System.out.println("4,8min  " + Math.min(4, 8));
+		System.out.println("-7 " + Math.abs(-7));
 	}
 }
