bdd6bc73b998ec17e44145ffb19f9f02b3b10474
kingwangz <540886982@qq.com>
2017-08-08 15:13:45 +0800

    BigInteger

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,14 +1,16 @@
 package king1;
 
-import java.util.Random;
+import java.math.BigInteger;;
 
 public class koing1 {
 	public static void main(String[] args) {
-		Random n = new Random();
-		System.out.println(n.nextInt());
-		System.out.println(n.nextInt(100));
-		System.out.println(n.nextFloat());
-		System.out.println(n.nextDouble());
-		System.out.println(n.nextBoolean());
+		BigInteger n = new BigInteger("4");
+		System.out.println(n.add(new BigInteger("2")));
+		System.out.println(n.subtract(new BigInteger("2")));
+		System.out.println(n.multiply(new BigInteger("2")));
+		System.out.println(n.divide(new BigInteger("2")));
+		System.out.println(n.divideAndRemainder(new BigInteger("3"))[1]);
+		System.out.println(n.pow(2));
+		System.out.println(n.negate());
 	}
 }
