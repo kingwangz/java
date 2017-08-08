dc39e72cb687cf49f2cd32029db1f0c87c127de9
kingwangz <540886982@qq.com>
2017-08-08 11:10:13 +0800

    Math

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -3,9 +3,10 @@ package king1;
 public class koing1 {
 
 	public static void main(String[] args) {
-		System.out.println("sin90 " + Math.sin(Math.PI / 2));
-		System.out.println("cos0 " + Math.cos(0));
-		System.out.println("tan90 " + Math.tan(Math.PI / 2));
-		System.out.println("120 " + Math.toRadians(120));
+		System.out.println(Math.exp(2));
+		System.out.println(Math.log(2));
+		System.out.println(Math.sqrt(4));
+		System.out.println(Math.cbrt(8));
+		System.out.println(Math.pow(4, 4));
 	}
 }
