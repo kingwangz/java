5a628271a8f989a9204dfbef1d8e955acc77ee93
kingwangz <540886982@qq.com>
2017-08-09 15:15:19 +0800

    构造方法和成员方法

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,16 +1,47 @@
 package king1;
 
 public class koing1 {
-	public static void main(String[] args) {
-		String v1 = "123";
-		String v2 = "123";
-		System.out.println(v1.equals(v2));
-		v s1 = new V();
-		v s2 = new V();
-		System.out.println(s1.equals(s2));
+	private String username;
+	private String password;
+
+	public koing1() {
+
 	}
-}
 
-class v {
+	public koing1(String name, String password) {
+		this.username = name;
+		this.password = password;
+	}
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public static void main(String[] args) {
+
+		koing1 u = new koing1();
+		System.out.println("用户名：" + u.username);
+		System.out.println("密码：" + u.password);
+
+		koing1 k = new koing1("zhangsan", "123456");
+		System.out.println("用户名：" + k.username);
+		System.out.println("密码：" + k.password);
+
+		k.setUsername("lisi");
+		k.setPassword("654321");
+		System.out.println("用户名：" + k.getUsername() + u.getUsername());
+		System.out.println("密码：" + k.getPassword() + u.getPassword());
+	}
 }
