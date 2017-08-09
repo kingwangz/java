d24d160f4a02f230774fb10f1b022d89f7b4291c
kingwangz <540886982@qq.com>
2017-08-09 16:25:13 +0800

    向上转型 静态

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,16 +2,18 @@ package king1;
 
 public class koing1 {
 	public static void n(koing1 q) {
-
+		System.out.println("king");
 	}
 
 	public static void main(String[] args) {
-		n(new K());
-		koing1 q = new k();
-		koing1 p = (k) q;
+		k o = new K();
+		n(o);
+		System.out.println("k" + o);
 	}
 }
 
 class k extends koing1 {
-
+	public static void n(k q) {
+		System.out.println("wang");
+	}
 }
