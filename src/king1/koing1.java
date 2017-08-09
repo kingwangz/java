bd6a7a5b6a38b04d2de6e8e676f970cf81600638
kingwangz <540886982@qq.com>
2017-08-09 13:51:36 +0800

    自定义类

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,6 +2,15 @@ package king1;
 
 public class koing1 {
 	public static void main(String[] args) {
-		System.out.println(new koing1());
+		String v1 = "123";
+		String v2 = "123";
+		System.out.println(v1.equals(v2));
+		v s1 = new V();
+		v s2 = new V();
+		System.out.println(s1.equals(s2));
 	}
+}
+
+class v {
+
 }
