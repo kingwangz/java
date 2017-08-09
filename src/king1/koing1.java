9344fa95c97fafb0019116d96dd2aebc3b0a8ee1
kingwangz <540886982@qq.com>
2017-08-09 13:45:25 +0800

    new

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,22 +1,7 @@
 package king1;
 
 public class koing1 {
-	koing1() {
-		System.out.println("k");
-	}
 	public static void main(String[] args) {
-		koing3 g = new koing3();
-	}
-}
-
-class koing2 extends koing1 {
-	koing2() {
-		System.out.println("i");
-	}
-}
-
-class koing3 extends koing2 {
-	koing3() {
-		System.out.println("n");
+		System.out.println(new koing1());
 	}
 }
