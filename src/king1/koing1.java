3bbe877e90043ac4128f9ddfb80099744ca32a34
kingwangz <540886982@qq.com>
2017-08-10 16:03:53 +0800

    向下转型

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,23 +1,22 @@
 package king1;
 
 public class koing1 {
-	void draw() {
-		System.out.println("Shape draw.");
-
+	public static void n(koing1 q) {
+		System.out.println("wang");
 	}
 
-	public static void drawInTest(koing1 s) {
-		s.draw();
-	}
-
-	public static void main(String args[]) {
-		Circle c = new Circle();
-		drawInTest(c);
+	public static void main(String[] args) {
+		n(new K());
+		koing1 q = new k();
+		System.out.println(q);
+		k p = (k) q;
+		System.out.println(p);
+		
 	}
 }
 
-class Circle extends koing1 {
-	void draw() {
-		System.out.println("Circle draw.");
+class k extends koing1 {
+	public k() {
+		System.out.println("king");
 	}
 }
