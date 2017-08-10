5459ecb52c30dcf0e3e4e5cd462311550dc657f5
kingwangz <540886982@qq.com>
2017-08-10 15:39:04 +0800

    向上转型 动态

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,32 +1,23 @@
 package king1;
 
 public class koing1 {
-	private koing1[] p = new koing1[6];
-	private int n = 0;
+	void draw() {
+		System.out.println("Shape draw.");
 
-	public void draw(koing1 q) {
-		if (n < p.length) {
-			p[n] = q;
-			System.out.println(n + p.length + q);
-			n++;
-		}
 	}
 
-	public static void main(String[] args) {
-		koing1 q = new koing1();
-		q.draw(new k());
-		q.draw(new sk());
+	public static void drawInTest(koing1 s) {
+		s.draw();
+	}
+
+	public static void main(String args[]) {
+		Circle c = new Circle();
+		drawInTest(c);
 	}
 }
 
-class sk extends koing1 {
-	public sk() {
-		System.out.println("king");
-	}
-}
-
-class k extends koing1 {
-	public k() {
-		System.out.println("wang");
+class Circle extends koing1 {
+	void draw() {
+		System.out.println("Circle draw.");
 	}
 }
