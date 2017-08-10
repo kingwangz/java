1205fbdaa8e67d7490df0ad0bb5c5dcda743a359
kingwangz <540886982@qq.com>
2017-08-10 17:49:33 +0800

    import

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,44 +1,10 @@
 package king1;
 
+import static java.lang.Math.max;
+import static java.lang.System.out;
+
 public class koing1 {
-	public void dot() {
-	}
-
 	public static void main(String[] args) {
-		dr[] d = { new pa(), new sq() };
-		for (int i = 0; i < d.length; i++) {
-			d[i].draw();
-		}
+		out.println(max(1, 3));
 	}
-}
-
-class an extends koing1 {
-	public void dot() {
-	}
-}
-
-interface dr {
-	public void draw();
-}
-
-class pa extends koing1 implements dr {
-	public void draw() {
-		System.out.println("king");
-	}
-
-	public void dot() {
-
-	}
-
-}
-
-class sq extends koing1 implements dr {
-	public void draw() {
-		System.out.println("wang");
-	}
-
-	public void dot() {
-
-	}
-
 }
