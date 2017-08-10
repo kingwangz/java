49d47abb4d428319fd586bd3c345d734057b18d2
kingwangz <540886982@qq.com>
2017-08-10 16:42:44 +0800

    接口

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,32 +1,44 @@
 package king1;
 
 public class koing1 {
-	private koing1[] p = new koing1[6];
-	private int n = 0;
-
-	public void draw(koing1 q) {
-		if (n < p.length) {
-			p[n] = q;
-			System.out.println(n + p.length);
-			n++;
-		}
+	public void dot() {
 	}
 
 	public static void main(String[] args) {
-		koing1 q = new koing1();
-		q.draw(new k());
-		q.draw(new sk());
+		dr[] d = { new pa(), new sq() };
+		for (int i = 0; i < d.length; i++) {
+			d[i].draw();
+		}
 	}
 }
 
-class sk extends koing1 {
-	public sk() {
+class an extends koing1 {
+	public void dot() {
+	}
+}
+
+interface dr {
+	public void draw();
+}
+
+class pa extends koing1 implements dr {
+	public void draw() {
 		System.out.println("king");
 	}
+
+	public void dot() {
+
+	}
+
 }
 
-class k extends koing1 {
-	public k() {
+class sq extends koing1 implements dr {
+	public void draw() {
 		System.out.println("wang");
 	}
+
+	public void dot() {
+
+	}
+
 }
