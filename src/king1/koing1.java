ec9b63a2fc8ea89186c244d93676db19a3f67241
kingwangz <540886982@qq.com>
2017-08-10 18:52:13 +0800

    final

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,10 +1,38 @@
 package king1;
 
-import static java.lang.Math.max;
 import static java.lang.System.out;
+import java.util.Random;
 
 public class koing1 {
-	public static void main(String[] args) {
-		out.println(max(1, 3));
+	static Random r = new Random();
+	static final int v = 9;
+	private static final int v2 = 10;
+	private final t test = new t();
+	private t test2 = new t();
+	private final int[] a = new int[] { 1, 2, 3, 4, 5, 6 };
+	private final int i = r.nextInt(20);
+	private static final int i2 = r.nextInt(20);
+
+	public String toString() {
+		return i + " " + i2 + " ";
 	}
+
+	public static void main(String[] args) {
+		koing1 da = new koing1();
+		// da.test=new t();
+		// da.v2++;
+		da.test2 = new T();
+		for (int i = 0; i < da.a.length; i++) {
+			// a[i]=9;
+		}
+		out.println(da);
+		out.println("data2");
+		out.println(new koing1());
+		out.println(da);
+
+	}
+}
+
+class t {
+
 }
