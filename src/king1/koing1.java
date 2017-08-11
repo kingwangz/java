0e887299b7e6f919d1479e8223b01f278950113a
kingwangz <540886982@qq.com>
2017-08-11 10:51:08 +0800

    final static

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -4,35 +4,16 @@ import static java.lang.System.out;
 import java.util.Random;
 
 public class koing1 {
-	static Random r = new Random();
-	static final int v = 9;
-	private static final int v2 = 10;
-	private final t test = new t();
-	private t test2 = new t();
-	private final int[] a = new int[] { 1, 2, 3, 4, 5, 6 };
-	private final int i = r.nextInt(20);
-	private static final int i2 = r.nextInt(20);
-
-	public String toString() {
-		return i + " " + i2 + " ";
-	}
+	private static Random rand = new Random();
+	private final int a = rand.nextInt(10);
+	private static final int a2 = rand.nextInt(10);
 
 	public static void main(String[] args) {
-		koing1 da = new koing1();
-		// da.test=new t();
-		// da.v2++;
-		da.test2 = new T();
-		for (int i = 0; i < da.a.length; i++) {
-			// a[i]=9;
-		}
-		out.println(da);
-		out.println("data2");
-		out.println(new koing1());
-		out.println(da);
-
+		koing1 fa = new koing1();
+		out.println(fa.a);
+		out.println(fa.a2);
+		koing1 faa = new koing1();
+		out.println(faa.a);
+		out.println(faa.a2);
 	}
-}
-
-class t {
-
 }
