4ba93bb1c048401fa19c181760e7d1aa668e4c70
kingwangz <540886982@qq.com>
2017-08-14 14:19:23 +0800

    内部类接口

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,50 +1,29 @@
 package king1;
 
-import static java.lang.System.out;
-
 public class koing1 {
-	public int u = 0;
-
-	class k {
-		int y = 0;
-
-		k() {
-			out.println("king");
-			//u=8;
-		}
-
-		public void kin() {
-			out.println("wang");
-		}
-
-		public void l() {
-			u = 9;
-		}
-
-	}
-
-	K in = new K();
-
-	public void ouf() {
-		in.kin();
-	}
-
-	public k doit() {
-		in.y = 4;
-		// u=9;
-		in.l();
-		out.println(+in.y);
-		return new K();
-	}
-
 	public static void main(String[] args) {
-		koing1 out = new koing1();
-		koing1.K i = out.doit();
-		koing1.K i2 = out.new K();
-		System.out.println(i);
-		System.out.println(i2);
-		System.out.println(+out.u);
-		int l = i.y;
-		System.out.println(l);
+		i out = new I();
+		in outi = out.doit();
+		outi.f();
+	}
+}
+
+interface in {
+	public void f();
+}
+
+class i {
+	private class on implements in {
+		on(String s) {
+			System.out.println(s);
+		}
+
+		public void f() {
+			System.out.println("king");
+		}
+	}
+
+	public in doit() {
+		return new on("wang");
 	}
 }
