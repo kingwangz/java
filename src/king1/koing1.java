3ff28591dd8deed3bb976b54d23947e364c41322
kingwangz <540886982@qq.com>
2017-08-14 14:58:19 +0800

    内部类this

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,29 +1,30 @@
 package king1;
 
 public class koing1 {
+	private int x;
+
+	private class in {
+		private int x = 9;
+
+		public void doit(int x) {
+			x++;
+			this.x++;
+			koing1.this.x++;
+		}
+	}
+
+	in w = new in();
+
+	public void e() {
+		W.doit(0);
+		System.out.println(+W.x);
+	}
+
 	public static void main(String[] args) {
-		i out = new I();
-		in outi = out.doit();
-		outi.f();
-	}
-}
+		koing1 p = new koing1();
+		p.e();
+		p.x = 1;
+		System.out.println(+p.x);
 
-interface in {
-	public void f();
-}
-
-class i {
-	private class on implements in {
-		on(String s) {
-			System.out.println(s);
-		}
-
-		public void f() {
-			System.out.println("king");
-		}
-	}
-
-	public in doit() {
-		return new on("wang");
 	}
 }
