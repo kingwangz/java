4c1b250c7755615e5b63b814f2d78c2ee380aa91
kingwangz <540886982@qq.com>
2017-08-14 15:58:12 +0800

    匿名内部类

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,22 +1,21 @@
 package king1;
 
 public class koing1 {
-	public on dot(final String x) {
-		class in implements on {
-			public in(String s) {
-				s = x;
-				System.out.println(s);
+	public on dot() {
+		return new on() {
+			private int i = 0;
+			public int get() {
+				return i;
 			}
-		}
-		return new in("dot");
+		};
 	}
 
 	public static void main(String[] args) {
 		koing1 pKoing1 = new koing1();
-		
-		pKoing1.dot("king");
+		pKoing1.dot();
 	}
 }
 
 interface on {
+
 }
