e0bd65f230c1dda42f8f8e466508917fa076cfa5
kingwangz <540886982@qq.com>
2017-08-15 15:50:22 +0800

    自定义异常

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -2,27 +2,30 @@ package king1;
 
 public class koing1 {
 	static int avg(int num1, int num2) throws myex {
-		if (num1 < 0 || num2 < 0) {
+		if (num2 < 0) {
 			throw new myex("king");
 		}
-		if (num1 > 100 || num2 > 100) {
-			throw new myex("wang");
-		}
-		return (num1 + num2) / 2;
+		return num1 / num2;
 	}
 
 	public static void main(String[] args) {
 		try {
-			int r = avg(102, 122);
+			int r = avg(3, -1);
 			System.out.println("k" + r);
 		} catch (myex e) {
-			System.out.println(e);
+			System.out.println(e.get());
 		}
 	}
 }
 
 class myex extends Exception {
+	String mess;
+
 	public myex(String error) {
-		super(error);
+		mess = error;
+	}
+
+	public String get() {
+		return mess;
 	}
 }
