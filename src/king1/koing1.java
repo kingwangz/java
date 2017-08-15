2fa11c9a09fc156b816acf8fd090d8fa15141660
kingwangz <540886982@qq.com>
2017-08-15 18:16:18 +0800

    JDialog窗口

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,24 +1,48 @@
 package king1;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class koing1 extends JFrame {
 	private static final long serialVersionUID = 1L;
 
-	public void cre(String tit) {
-		JFrame jf = new JFrame();
+	public koing1(String tit) {
+		JFrame jf = new JFrame(tit);
 		Container container = jf.getContentPane();
-		JLabel jl = new JLabel("king");
+		JLabel jl = new JLabel("king  dfdf");
+		container.setLayout(null);
 		jl.setHorizontalAlignment(SwingConstants.CENTER);
 		container.add(jl);
 		container.setBackground(Color.white);
-		jf.setVisible(true);
+		jf.setVisible(true); 
 		jf.setSize(600, 550);
+		JButton bl = new JButton("on");
+		bl.setBounds(10, 10, 100, 21);
+		bl.setVisible(true);
+		bl.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new myj(koing1.this).setVisible(true);
+			}
+		});
+		container.add(bl);
 		jf.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
 	public static void main(String[] args) {
-		new koing1().cre("king");
+		new koing1("k");
+	}
+}
+
+class myj extends JDialog {
+	private static final long serialVersionUID = 1L;
+
+	public myj(koing1 koing1) {
+		super();
+		Container container = getContentPane();
+		container.add(new JLabel("wang"));
+		setBounds(120, 120, 100, 100);
 	}
 }
