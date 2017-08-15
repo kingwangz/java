df954839e031628145ee627ac0cff4d723e1b571
kingwangz <540886982@qq.com>
2017-08-15 16:16:12 +0800

    JFrame窗体

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,31 +1,24 @@
 package king1;
 
-public class koing1 {
-	static int avg(int num1, int num2) throws myex {
-		if (num2 < 0) {
-			throw new myex("king");
-		}
-		return num1 / num2;
+import java.awt.*;
+import javax.swing.*;
+
+public class koing1 extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	public void cre(String tit) {
+		JFrame jf = new JFrame();
+		Container container = jf.getContentPane();
+		JLabel jl = new JLabel("king");
+		jl.setHorizontalAlignment(SwingConstants.CENTER);
+		container.add(jl);
+		container.setBackground(Color.white);
+		jf.setVisible(true);
+		jf.setSize(600, 550);
+		jf.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
 	public static void main(String[] args) {
-		try {
-			int r = avg(3, -1);
-			System.out.println("k" + r);
-		} catch (myex e) {
-			System.out.println(e.get());
-		}
-	}
-}
-
-class myex extends Exception {
-	String mess;
-
-	public myex(String error) {
-		mess = error;
-	}
-
-	public String get() {
-		return mess;
+		new koing1().cre("king");
 	}
 }
