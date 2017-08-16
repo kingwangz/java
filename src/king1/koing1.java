6f588521ec47c36d0fe8de87458cca5e1a251a3e
kingwangz <540886982@qq.com>
2017-08-16 14:19:03 +0800

    图片图标

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,43 +1,31 @@
 package king1;
 
 import java.awt.*;
+import java.net.*;
 
 import javax.swing.*;
 
-public class koing1 extends JFrame implements Icon {
+public class koing1 extends JFrame {
 	private static final long serialVersionUID = 1L;
-	private int width;
-	private int height;
 
-	public int getIconHeight() {
-		return this.height;
-	}
-
-	public int getIconWidth() {
-		return this.width;
-	}
-
-	public koing1(int width, int height) {
-		this.width = width;
-		this.height = height;
-	}
-
-	public void paintIcon(Component arg0, Graphics arg1, int x, int y) {
-		arg1.fillOval(x, y, width, height);
-	}
-
-	public static void main(String[] args) {
-		koing1 icon = new koing1(15, 15);
+	public koing1() {
 		JFrame jf = new JFrame();
+		JLabel jl = new JLabel("king");
 		Container container = jf.getContentPane();
-		JLabel jl = new JLabel("king", icon, SwingConstants.CENTER);
-		jl.setHorizontalAlignment(SwingConstants.CENTER);
+		URL url = koing1.class.getResource("imagebutton.jpg");
+		Icon icon = new ImageIcon(url);	
+		jl.setIcon(icon);
+		jl.setHorizontalAlignment(SwingConstants.CENTER);	
 		container.add(jl);
+		jl.setOpaque(true);
 		container.setBackground(Color.white);
 		jf.setVisible(true);
 		jf.setSize(600, 550);
 		jf.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		icon.paintIcon(null, null, 15, 15);
+	}
+
+	public static void main(String[] args) {
+		new koing1();
 	}
 
 }
