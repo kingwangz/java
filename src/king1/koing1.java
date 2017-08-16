ce7a387bb67c9930538e31ee6c94776e3ee5be32
kingwangz <540886982@qq.com>
2017-08-16 15:38:58 +0800

    边界布局管理器

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -7,15 +7,18 @@ import javax.swing.*;
 
 public class koing1 extends JFrame {
 	private static final long serialVersionUID = 1L;
+	String[] borde = new String[] { BorderLayout.CENTER, BorderLayout.NORTH, BorderLayout.SOUTH, BorderLayout.WEST,
+			BorderLayout.EAST };
+	String[] bn = new String[] { "center", "north", "south", "weat", "east" };
 
 	public koing1() {
 		setTitle("king");
 		Container container = getContentPane();
-		setLayout(new FlowLayout(2, 10, 10));
-		for (int i = 0; i < 10; i++) {
-			container.add(new JButton("king" + i));
+		setLayout(new BorderLayout());
+		for (int i = 0; i < borde.length; i++) {
+			container.add(borde[i], new JButton(bn[i]));
 		}
-		setSize(300, 300);
+		setSize(350, 200);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
