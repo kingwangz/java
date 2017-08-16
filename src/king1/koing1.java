6cbf9e4f902c4c9e96fdbe2741764ed4379e81db
kingwangz <540886982@qq.com>
2017-08-16 11:09:52 +0800

    Icon

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -1,48 +1,43 @@
 package king1;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class koing1 extends JFrame {
+public class koing1 extends JFrame implements Icon {
 	private static final long serialVersionUID = 1L;
+	private int width;
+	private int height;
 
-	public koing1(String tit) {
-		JFrame jf = new JFrame(tit);
-		Container container = jf.getContentPane();
-		JLabel jl = new JLabel("king");
-		container.setLayout(null);
-		jl.setHorizontalAlignment(SwingConstants.CENTER);
-		container.add(jl);
-		container.setBackground(Color.white);
-		jf.setVisible(true); 
-		jf.setSize(600, 550);
-		JButton bl = new JButton("on");
-		bl.setBounds(10, 10, 100, 21);
-		bl.setVisible(true);
-		bl.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				new myj(koing1.this).setVisible(true);
-			}
-		});
-		container.add(bl);
-		jf.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	public int getIconHeight() {
+		return this.height;
+	}
+
+	public int getIconWidth() {
+		return this.width;
+	}
+
+	public koing1(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	public void paintIcon(Component arg0, Graphics arg1, int x, int y) {
+		arg1.fillOval(x, y, width, height);
 	}
 
 	public static void main(String[] args) {
-		new koing1("k");
+		koing1 icon = new koing1(15, 15);
+		JFrame jf = new JFrame();
+		Container container = jf.getContentPane();
+		JLabel jl = new JLabel("king", icon, SwingConstants.CENTER);
+		jl.setHorizontalAlignment(SwingConstants.CENTER);
+		container.add(jl);
+		container.setBackground(Color.white);
+		jf.setVisible(true);
+		jf.setSize(600, 550);
+		jf.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		icon.paintIcon(null, null, 15, 15);
 	}
-}
 
-class myj extends JDialog {
-	private static final long serialVersionUID = 1L;
-
-	public myj(koing1 koing1) {
-		super();
-		Container container = getContentPane();
-		container.add(new JLabel("wang"));
-		setBounds(120, 120, 100, 100);
-	}
 }
