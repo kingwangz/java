c99d6df826d2f057e4be28a6fc57ccec8af8436e
kingwangz <540886982@qq.com>
2017-08-16 14:52:26 +0800

    绝对布局

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -9,19 +9,18 @@ public class koing1 extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	public koing1() {
-		JFrame jf = new JFrame();
-		JLabel jl = new JLabel("king");
-		Container container = jf.getContentPane();
-		URL url = koing1.class.getResource("imagebutton.jpg");
-		Icon icon = new ImageIcon(url);	
-		jl.setIcon(icon);
-		jl.setHorizontalAlignment(SwingConstants.CENTER);	
-		container.add(jl);
-		jl.setOpaque(true);
-		container.setBackground(Color.white);
-		jf.setVisible(true);
-		jf.setSize(600, 550);
-		jf.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("king");
+		setLayout(null);
+		setBounds(0, 0, 200, 150);
+		Container container = getContentPane();
+		JButton b1 = new JButton("1");
+		JButton b2 = new JButton("2");
+		b1.setBounds(10, 30, 80, 30);
+		b2.setBounds(60, 70, 100, 20);
+		container.add(b1);
+		container.add(b2);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
 	public static void main(String[] args) {
