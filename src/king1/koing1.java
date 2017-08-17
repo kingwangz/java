14ad76067cac6c1776291759793d61eb4b9a8d6d
kingwangz <540886982@qq.com>
2017-08-17 10:39:01 +0800

    JFrame模板

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -9,15 +9,13 @@ import javax.swing.*;
 public class koing1 extends JFrame {
 	private static final long serialVersionUID = 1L;
 	public koing1() {
-		JLabel jLabel= new JLabel("king");
-		JFrame  jFrame =new JFrame("k");
-		jLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		jFrame.setVisible(true);
-		jFrame.setSize(250,240);
-		Container container=jFrame.getContentPane();
+		setTitle("king");
+		JLabel jLabel=new JLabel("wang");
+		Container container=getContentPane();
 		container.add(jLabel);
-		container.setBackground(Color.white);
-		jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(250,250);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	public static void main(String[] args) {
 		new koing1();
