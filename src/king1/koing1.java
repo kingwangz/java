c01b461f9f3aa163a2fdeca5bd94eaa6cb0ff4b0
kingwangz <540886982@qq.com>
2017-08-17 11:10:28 +0800

    单选按钮组件

--- src/king1/koing1.java
+++ src/king1/koing1.java
@@ -5,18 +5,26 @@ import java.awt.*;
 import java.net.*;
 
 import javax.swing.*;
+import javax.xml.soap.Text;
 
 public class koing1 extends JFrame {
 	private static final long serialVersionUID = 1L;
+
 	public koing1() {
-		setTitle("king");
-		JLabel jLabel=new JLabel("wang");
-		Container container=getContentPane();
-		container.add(jLabel);
-		setSize(250,250);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		JLabel jLabel = new JLabel("king");
+		JFrame jFrame = new JFrame("k");
+		JRadioButton jRadioButton = new JRadioButton("koing");
+		//ButtonGroup buttonGroup = new ButtonGroup();
+		jLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		jFrame.setVisible(true);
+		jFrame.setSize(250, 240);
+		Container container = jFrame.getContentPane();
+		container.setBackground(Color.white);
+		//buttonGroup.add(jRadioButton);
+		container.add(jRadioButton);
+		jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
+
 	public static void main(String[] args) {
 		new koing1();
 	}
